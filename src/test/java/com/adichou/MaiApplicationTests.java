e12595bcf1a80916ff3a6b08078ea8c318803794
adichou <adichou501@126.com>
2017-04-23 20:27:23 -0700

    调整为Spring-boot项目
    Signed-off-by: ADICHOU <ADICHOU@adis-MacBook-Pro.local>

---
+++ src/test/java/com/adichou/MaiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adichou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MaiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
