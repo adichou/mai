9afad75d166d4d6d0e7319fae2d6c8a823153120
ADICHOU <ADICHOU@adis-MacBook-Pro.local>
2017-04-24 19:44:35 -0700

    第一个应用程序
    Signed-off-by: ADICHOU <ADICHOU@adis-MacBook-Pro.local>

--- src/main/java/com/adichou/iam/controller/MasterController.java
+++ src/main/java/com/adichou/iam/controller/MasterController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
+/*
  * Created by ADICHOU on 2017/4/23.
  */
 @RestController
